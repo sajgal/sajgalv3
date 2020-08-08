02280febdb56c1b943858ef26b6dc814b4ce5d76
Matej Sajgal <roarbb@gmail.com>
2020-08-08 17:20:41 +1000

    Landing to Index page cleanup, media library cleanup

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,9 +3,6 @@ import CMS from 'netlify-cms-app'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 import PortfolioPreview from './preview-templates/PortfolioPreview'
-// import { setupMediaLibrary } from './mediaLibrary';
-
-// setupMediaLibrary();
 
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
 CMS.registerPreviewTemplate('index', IndexPagePreview)
--- src/pages/landing.md
+++
@@ -1,9 +0,0 @@
----
-templateKey: landing-page
-title: Matej Sajgal
-images:
-  - image: /img/dscf9220.jpg
-  - image: /img/dscf8730.jpg
-  - image: /img/dscf8519.jpg
----
-If you're looking for my photos, [they are here.](http://www.theyarehere.com)
