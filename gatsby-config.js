7e65d8abdea619b07cd058e57d91f6fb314cf357
Matej Sajgal <roarbb@gmail.com>
2020-08-18 22:31:05 +1000

    Updates site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Matej Sajgal',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Matej Sajgal - web developer, photographer. Available for hire.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
